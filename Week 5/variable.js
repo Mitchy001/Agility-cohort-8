ab5e6d1ab2dff362b165f67e9a4da36e851f205d
Mitchy001 <kemuntomitchell0@gmail.com>
2025-03-18 13:33:40 +0300

    frontend

---
+++ Week 5/variable.js
@@ -0,0 +1,41 @@
+// Declaring variables with different data types
+let stringVariable = "Hello, JavaScript"; // String
+let booleanVariable = true;               // Boolean
+let myUndefinedVariable;                  // Undefined
+let nullVariable = null;                  // Null
+
+let firstName = "Michel";
+let lastName = "Mayenga";
+let maritalStatus = "Single";
+let age = 25;
+
+let myAge = 25;
+let yourAge = 28;
+
+// Corrected spacing in console.log
+console.log('I am ' + myAge + ' years old');  // Added space before "years"
+console.log('You are ' + yourAge + ' years old'); // Added space and capitalized "You"
+
+
+
+let str='Learning JavaScript'
+str.toUpperCase()
+
+console.log(str) 
+
+
+// reassign on method call in it
+str=str.toUpperCase()
+
+console.log(str)
+
+str=str.toLowerCase()
+console.log(str) 
+
+console.log(str.includes("a"))
+
+
+// indexes-position of character in your string 
+// indexe statrts with 0
+
+console.log(str[0])
---
+++ Week 7/calculator.js
@@ -0,0 +1,7 @@
+
+// creating and adding elements
+const newParagraph =document.createElement("p");
+newParagraph.textContent =("newParagraph");
+
+// const newHeading =document.createElement("h4");
+// newHeading.textContent =("new tag here");
