8c54c99e48ec1c87e6ab3df1f9b7c78d7edd0505
Mitchy001 <kemuntomitchell0@gmail.com>
2025-04-08 05:57:46 +0300

    changes

--- Week 7/calculator.js
+++ Week 7/calculator.js
@@ -2,6 +2,7 @@
 // creating and adding elements
 const newParagraph =document.createElement("p");
 newParagraph.textContent =("newParagraph");
+document.getElementById("container").appendChild(newParagraph);
 
 // const newHeading =document.createElement("h4");
 // newHeading.textContent =("new tag here");
